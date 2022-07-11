ac5d60df07437dbd907deba61b622ebe1eb649b7
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-07-11 14:50:30 +0200

    Update main.c

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -3,35 +3,23 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 
-// static char *str = NULL;
-// while  (ya pas de /n dans str)
-// 	buffer = read(fd)
-// 	strjoin(str, buffer 1)
-// 	free(buffer) //free
-// "xxxxxx\nxx\nx\nxxx\nxx" str
-// il faut que tu envoie "xxxxxx\n" la premiere
-// '\n' la deuxieme etc..
-// str_final= copie tout str jusquau premier \n inclue
-// str = devient "xx\nx\nxxx\nxx" //free
-// 3 fonctions:
-// une qui strjoin les buffer jusqua avoir un \n
-// une qui prepapre la str a renvoyer (malloc et copie jusqua 1er /n)
-// une qui prepare la str dans le futur (copie tout apres le 1er\n et free la version precedente)
-// ger la fin du fichier;
-// return(str_final)
+// gerer la fin du fichier;
 
 int	main	(void)
 {
 	int		fd;
-	char	*str;
+	char	*s;
 
-	fd= open("test.txt", O_RDONLY);
-	str = get_next_line(fd);
-	// while(str)
-	// {
-	// 	printf("%s\n", str);
-	// 	free(str);
-	// 	str = get_next_line(fd);
-	// }
+	fd = open("test.txt", O_RDONLY);
+	// s = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	// printf("%lu\n", read(fd, s, BUFFER_SIZE));
+	s = get_next_line(fd);
+	printf("%s\n", s);
+	while(s)
+	{
+		s = get_next_line(fd);
+		printf("%s", s);
+		free(s);
+	}
 	return (0);
-}	
+}
