f4cfbee32a2c3cffe90b6d5e034adfa30ffb97ba
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-07-11 14:50:07 +0200

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -8,7 +8,7 @@
 char	*get_next_line(int fd);
 size_t	ft_strlen(char *s);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strcop(char *str, size_t len, size_t slen);
+char	*ft_strcop(char *str);
 char	*ft_strfinal(char *str);
 int	ft_non(char *str);
 
