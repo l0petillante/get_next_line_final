acccc5f29cf630998493620339db760f8539fb41
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-07-11 14:48:58 +0200

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -13,8 +13,8 @@ size_t	ft_strlen(char *s)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*res;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
@@ -25,68 +25,66 @@ char	*ft_strjoin(char *s1, char *s2)
 	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
-	printf("avant :%s\n",s2);
 	while (s1[i])
 	{
 		res[i] = s1[i];
 		i++;
 	}
-	printf("apres :%s\n",s2);
 	while (s2[j])
 	{
-		res[i] = s2[j];
-		i++;
-		j++;
+		res[i++] = s2[j++];
 	}
 	res[i] = '\0';
 	return (res);
 }
 
-// fonction qui copie une str dans un str final jusqu'au premier \n
-
 char	*ft_strfinal(char *str)
 {
-	char	*strfinal;
+	char	*res;
 	int		i;
 	int		j;
 
 	i = 0;
 	j = 0;
-	while (str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 		i++;
-	strfinal = malloc(sizeof(char) * i + 1);
-	if (!strfinal)
-	{
-		free(strfinal);
+	res = malloc(sizeof(char) * (i + 2));
+	if (!res)
 		return (NULL);
-	}
 	while (i >= 0)
 	{
-		strfinal[j] = str[j];
+		res[j] = str[j];
 		j++;
 		i--;
 	}
-	strfinal[i] = '\0';
-	return (strfinal);
+	res[j] = '\0';
+	return (res);
 }
 
-// fonction qui copie str à partir du premier \n
-char	*ft_strcop(char *str, size_t len, size_t slen)
+char	*ft_strcop(char *str)
 {
 	size_t	i;
 	size_t	j;
 	char	*res;
 
-	i = slen;
+	i = 0;
 	j = 0;
-	res = malloc(sizeof(char) * (len - slen + 1));
+	while (str[i] && str[i] != '\n')
+		i++;
+	res = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	i++;
 	if (!res)
+	{
+		free (res);
 		return (NULL);
-	while (j < len)
+	}
+	while (str[i])
 	{
 		res[j] = str[i];
 		j++;
 		i++;
 	}
+	res[j] = '\0';
+	free(str);
 	return (res);
 }
