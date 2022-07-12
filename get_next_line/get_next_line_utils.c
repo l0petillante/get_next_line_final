a781bf00eb38fabc299b32fb1861dbb6db46685f
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-07-12 20:30:29 +0200

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhmissi <lhmissi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 14:02:21 by lhmissi           #+#    #+#             */
+/*   Updated: 2022/07/12 19:33:52 by lhmissi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 size_t	ft_strlen(char *s)
@@ -8,6 +20,28 @@ size_t	ft_strlen(char *s)
 	while (s[i] != '\0')
 		i++;
 	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	size;
+	size_t	i;
+	char	*res;
+
+	size = ft_strlen(s) + 1;
+	i = 0;
+	res = malloc(sizeof(char) * size);
+	if (!res)
+		return (NULL);
+	else
+	{
+		while (i < size)
+		{
+			res[i] = s[i];
+			i++;
+		}
+	}
+	return (res);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
@@ -21,7 +55,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	if (!s2)
 		return (s1);
 	if (!s1)
-		return (ft_strjoin("", s2));
+		return (ft_strdup(s2));
 	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
@@ -35,6 +69,7 @@ char	*ft_strjoin(char *s1, char *s2)
 		res[i++] = s2[j++];
 	}
 	res[i] = '\0';
+	free(s1);
 	return (res);
 }
 
@@ -46,6 +81,8 @@ char	*ft_strfinal(char *str)
 
 	i = 0;
 	j = 0;
+	if (!str)
+		return (NULL);
 	while (str[i] && str[i] != '\n')
 		i++;
 	res = malloc(sizeof(char) * (i + 2));
@@ -69,21 +106,21 @@ char	*ft_strcop(char *str)
 
 	i = 0;
 	j = 0;
+	if (!str)
+		return (NULL);
 	while (str[i] && str[i] != '\n')
 		i++;
+	if (str[i] == '\0')
+	{
+		free (str);
+		return (NULL);
+	}
 	res = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
 	i++;
 	if (!res)
-	{
-		free (res);
 		return (NULL);
-	}
 	while (str[i])
-	{
-		res[j] = str[i];
-		j++;
-		i++;
-	}
+		res[j++] = str[i++];
 	res[j] = '\0';
 	free(str);
 	return (res);
