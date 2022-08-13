c0ffae6e8c858eec6d04370d57981e4a73c0158c
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-08-13 18:07:32 +0200

    Update get_next_line_utils.c
    strjoin trop longue

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lhmissi <lhmissi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 14:02:21 by lhmissi           #+#    #+#             */
-/*   Updated: 2022/07/19 16:21:41 by lhmissi          ###   ########.fr       */
+/*   Updated: 2022/08/13 17:45:41 by lhmissi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,35 @@ size_t	ft_strlen(char *s)
 char	*ft_strdup(char *s)
 {
 	size_t	size;
-	size_t	i;
+	int		i;
 	char	*res;
 
-	size = ft_strlen(s) + 1;
+	size = ft_strlen(s);
 	i = 0;
-	if (!s || size <= 1)
+	if (!s || size == 0)
 		return (NULL);
-	res = malloc(sizeof(char) * size);
-	if (!res )
+	res = malloc(sizeof(char) * size + 1);
+	if (!res)
+	{
+		free(res);
 		return (NULL);
+	}
 	else
 	{
-		while (i < size)
+		while (size > 0)
 		{
 			res[i] = s[i];
 			i++;
+			size --;
 		}
+		res[i] = '\0';
 	}
 	return (res);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
-	char	*res;
+	char	*re;
 	int		i;
 	int		j;
 
@@ -56,23 +61,24 @@ char	*ft_strjoin(char *s1, char *s2)
 	j = 0;
 	if (!s2 && s1)
 		return (s1);
-	if (!s1 && s2)
+	else if (!s1 && s2)
 		return (ft_strdup(s2));
-	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!res || (ft_strlen(s1) + ft_strlen(s2) <= 0))
+	re = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!re || ((ft_strlen(s1) + ft_strlen(s2)) == 0))
+	{
+		free(re);
 		return (NULL);
+	}
 	while (s1[i])
 	{
-		res[i] = s1[i];
+		re[i] = s1[i];
 		i++;
 	}
 	while (s2[j])
-	{
-		res[i++] = s2[j++];
-	}
-	res[i] = '\0';
+		re[i++] = s2[j++];
+	re[i] = '\0';
 	free(s1);
-	return (res);
+	return (re);
 }
 
 char	*ft_strfinal(char *str)
@@ -100,31 +106,28 @@ char	*ft_strfinal(char *str)
 	return (res);
 }
 
-char	*ft_strcop(char *str)
+char	*ft_strcop(char *str, size_t len)
 {
 	size_t	i;
 	size_t	j;
 	char	*res;
 
-	i = 0;
+	i = len;
 	j = 0;
-	if (!str)
+	if (!str || len == 0)
 		return (NULL);
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\0')
+	if (ft_strlen(str) == i)
 	{
 		free (str);
 		return (NULL);
 	}
 	res = malloc(sizeof(char) * (ft_strlen(str) - i + 1));
-	i++;
-	if (!res || ft_strlen(str) - i <= 0)
+	if (!res)
 	{
 		free (res);
 		return (NULL);
 	}
-	while (str[i])
+	while (res && str[i])
 		res[j++] = str[i++];
 	res[j] = '\0';
 	free(str);
