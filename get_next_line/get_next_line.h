61f24170d0aaa2147035e55df83b90aae48da5d7
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-08-15 13:52:36 +0200

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lhmissi <lhmissi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/11 14:02:31 by lhmissi           #+#    #+#             */
-/*   Updated: 2022/08/13 16:42:49 by lhmissi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -18,13 +6,12 @@
 # include <stdlib.h>
 
 int		ft_non(char *str);
-void	ft_bzero(void *s, size_t n);
 char	*ft_str(int fd, char *str, char *buffer);
 char	*get_next_line(int fd);
 size_t	ft_strlen(char *s);
 char	*ft_strdup(char *s);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strcop(char *str,size_t len);
+char	*ft_strcop(char *str, size_t len);
 char	*ft_strfinal(char *str);
 
 #endif
