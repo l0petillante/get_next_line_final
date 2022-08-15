af0fe49a34912a7a8dac85cd0ec64d36d6c6f314
l0petillante <76171304+l0petillante@users.noreply.github.com>
2022-08-15 13:53:03 +0200

    Update get_next_line_bonus.h

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lhmissi <lhmissi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/11 14:02:31 by lhmissi           #+#    #+#             */
-/*   Updated: 2022/08/13 17:39:23 by lhmissi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -18,7 +6,6 @@
 # include <stdlib.h>
 
 int		ft_non(char *str);
-void	ft_bzero(void *s, size_t n);
 char	*ft_str(int fd, char *str, char *buffer);
 char	*get_next_line(int fd);
 size_t	ft_strlen(char *s);
